8325da1a68f3766fe7da1a2be439e642716f9094
Claire Froelich <claire.froelich@gmail.com>
2020-10-04 16:44:39 -0700

    Add weighted quick union

---
+++ QuickUnionWeightedUF.java
@@ -0,0 +1,39 @@
+public class QuickUnionWeightedUF {
+    private int[] id;
+    private int[] sz;
+
+    public QuickUnionWeightedUF(int N) {
+        id = new int[N];
+        sz = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    private int root(int i) {
+        // chase parent pointers until reach root
+        while (i != id[i])
+            i = id[i];
+        return i;
+    }
+
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        if (i == j)
+            return;
+        // link root of smaller tree to root of larger tree, update sz[]
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
+    }
+}
